96fb28aa353038ec1e15b197b4d61638d16c0d6f
Antonio Pires <tonan@outlook.com.br>
2022-03-18 10:56:09 -0300

    210. Múltiplos mixins

--- src/components/ListaVideogames.vue
+++ src/components/ListaVideogames.vue
@@ -7,14 +7,16 @@
         <ul>
             <li v-for="(item, index) in itens" :key="index">{{ item }}</li>
         </ul>
+        <button type="button" @click="removerItem()">Remover</button>
     </div>
 </template>
 
 <script>
 import ListaMixin from '@/mixins/ListaMixin.js';
+import RemoverItemListaMixin from '@/mixins/RemoverItemListaMixin.js';
 
 export default {
     name: 'ListaVideogames',
-    mixins: [ListaMixin]
+    mixins: [ ListaMixin, RemoverItemListaMixin ]
 }
 </script>
